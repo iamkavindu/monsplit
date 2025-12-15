83e0b8735d9a68737be1af918961906accfa1f25
Kavindu Perera <59706267+iamkavindu@users.noreply.github.com>
2025-12-15 22:59:46 +0530

    Merge pull request #1
    * Add application configuration, database setup, and event publication flyway migration
    
    * Refactor application and test files for cleaner code structure

--- src/main/java/dev/iamkavindu/monsplit/MonsplitApplication.java
+++ src/main/java/dev/iamkavindu/monsplit/MonsplitApplication.java
@@ -1,13 +1,13 @@
 package dev.iamkavindu.monsplit;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonsplitApplication {
+public class MonsplitApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(MonsplitApplication.class, args);
     }
-
 }
--- src/test/java/dev/iamkavindu/monsplit/TestMonsplitApplication.java
+++ src/test/java/dev/iamkavindu/monsplit/TestMonsplitApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 public class TestMonsplitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(MonsplitApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(MonsplitApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
     }
-
 }
